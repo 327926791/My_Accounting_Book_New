13c2cec5d7e380c90524e2425c84b33f2da6d496
Mike <countrygtm@gmail.com>
2019-01-13 11:04:33 -0500

    csv generation, account category modification, and graph generation half done

--- My_Accounting_Book/ViewControllers/ViewController.swift
+++ My_Accounting_Book/ViewControllers/ViewController.swift
@@ -47,8 +47,35 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         
+        /*
+         TO CHI ZHANG
+         - place following in viewDidLoad in initial Scene
+         - set arrays in Transaction to
+            - accounts -> userDefaultAccounts
+            - categories -> userDefaultCategories
+            - incomeCategories -> not changed in any UI implement by Mike, could use original
+         Check if first launch
+         first launch:      store defaults categories and accounts to UserDefaults
+         not first launch:  read categories and accounts from UserDefaults
+         */
         
-        
+//        let launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
+//        if launchedBefore  {
+//            // not first launch
+//            userDefaultAccounts = UserDefaults.standard.stringArray(forKey: "accounts") ?? [String]()
+//            userDefaultCategories = UserDefaults.standard.stringArray(forKey: "categories") ?? [String]()
+//            userDefaultIncomeCategories = UserDefaults.standard.stringArray(forKey: "incomeCategories") ?? [String]()
+//        } else {
+//            // first launch
+//            UserDefaults.standard.set(true, forKey: "launchedBefore")
+//            userDefaultAccounts = ["Cash", "Debit Card", "Credit Card"]
+//            userDefaultCategories = ["Food and Drink", "Apparel", "Rent", "Loan and Mortgage", "Bill", "Transportation", "Travelling", "Entertainment", "Health and Fitness", "Education", "Grocery", "Shopping", "Gift", "Online Shopping", "Other"]
+//            userDefaultIncomeCategories = ["Salary and Wage", "Business Profit", "Investment Return", "Bank Interest", "Payment Received", "Other"]
+//
+//            UserDefaults.standard.set(userDefaultAccounts, forKey: "accounts")
+//            UserDefaults.standard.set(userDefaultCategories, forKey: "categories")
+//            UserDefaults.standard.set(userDefaultIncomeCategories, forKey: "incomeCategories")
+//        }
         
         
         // create db entries
